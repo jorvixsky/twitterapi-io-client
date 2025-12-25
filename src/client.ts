8d2309c86972e844c64c0e2ad2fab306402a4ecd
jorvixsky <jorvixsky@proton.me>
2025-12-25 11:11:32 +0100

    feat: added support for list api

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,19 @@
 import { HttpClient, type HttpClientOptions } from "./httpClient";
 import { UsersApi } from "./resources/users";
 import { MyEndpointApi } from "./resources/myEndpoint";
+import { ListApi } from "./resources/list";
 
 export interface TwitterAPIIOClientOptions extends HttpClientOptions { }
 
 export class TwitterAPIIOClient {
     public readonly users: UsersApi;
     public readonly myEndpoint: MyEndpointApi;
+    public readonly list: ListApi;
 
     constructor(options: TwitterAPIIOClientOptions) {
         const http = new HttpClient(options);
         this.users = new UsersApi(http);
         this.myEndpoint = new MyEndpointApi(http);
+        this.list = new ListApi(http);
     }
 }
